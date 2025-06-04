d9878105bb0310a8e46ae307a36c93fd1ff71579
Anita Kusumasari <anita.kusumasari@zettacamp.pro>
2025-06-04 14:12:51 +0700

    Update typeDefs
    CRUD Implementation

--- schema/typeDefs.js
+++ schema/typeDefs.js
@@ -1,27 +1,54 @@
-type User {
+type Student {
   id: ID!
   name: String!
   email: String!
+  school: School
 }
 
-input CreateUserInput {
+input CreateStudentInput {
   name: String!
   email: String!
+  schoolId: ID!
 }
 
-input UpdateUserInput {
+input UpdateStudentInput {
   id: ID!
   name: String
   email: String
+  schoolId: ID
 }
 
-type Query {
-  getAllUsers: [User!]!
-  getUserById(id: ID!): User
+type School {
+  id: ID!
+  name: String!
+  address: String!
+  students: [Student!]!
 }
 
-type Mutation {
-  createUser(input: CreateUserInput!): User!
-  updateUser(input: UpdateUserInput!): User!
-  deleteUser(id: ID!): Boolean!
+input CreateSchoolInput {
+  name: String!
+  address: String!
+}
+
+input UpdateSchoolInput {
+  id: ID!
+  name: String
+  address: String
+}
+
+extend type Query {
+  getAllStudents: [Student!]!
+  getStudentById(id: ID!): Student
+  getAllSchools: [School!]!
+  getSchoolById(id: ID!): School
+}
+
+extend type Mutation {
+  createStudent(input: CreateStudentInput!): Student!
+  updateStudent(input: UpdateStudentInput!): Student!
+  deleteStudent(id: ID!): Boolean!
+
+  createSchool(input: CreateSchoolInput!): School!
+  updateSchool(input: UpdateSchoolInput!): School!
+  deleteSchool(id: ID!): Boolean!
 }
